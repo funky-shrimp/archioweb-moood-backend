1ef5b02d15b4c4b43f495a5797479ded17aec76d
Funky Shrimp <thibaud.nuss@gmail.com>
2026-01-10 14:30:16 +0100

    Added welcome message on / endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,12 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("Ignition!");
+
+  //respond with a simple message and link to API documentation
+
+  res.send(`<h1>Welcome to the Moood Backend API</h1>
+    <p>Explore the API documentation at <a href="/api/docs">/api-docs</a></p>
+  `);
 });
 
 export default router;
